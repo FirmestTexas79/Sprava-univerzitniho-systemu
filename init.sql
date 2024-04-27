6c1b770d44e945a478fde1db14e454cc8617abe9
FirmestTexas79 <matasovsky.samuel@seznam.cz>
2024-04-27 20:46:25 +0200

    Css

--- init.sql
+++ init.sql
@@ -4,11 +4,11 @@ FLUSH PRIVILEGES;
 -- password: Password1
 INSERT INTO `users` (`id`, `firstname`, `lastname`, `password`, `email`, `birthdate`, `role`, `sex`, `createdAt`,
                      `updatedAt`)
-VALUES ('clvh9c4t30004356x7bandnpjZ', 'Vlastimil', 'Bureš',
+VALUES ('clvh9c4t31004356x7bandnpjZ', 'Vlastimil', 'Bureš',
         '$argon2id$v=19$m=65536,t=3,p=4$dBtduH+22brzrY3Ogp+fXA$vkpdi9+F7f9Yi1kD45LCtoO1PVv8r8Rq2M0JFT0ch3o',
-        'vlasaburak@seznam.cz', '2000-11-02 00:00:00.000', 'ADMIN', 'MALE', '2021-05-02 00:00:00.000',
+        'vlastaburak@seznam.cz', '2000-11-02 00:00:00.000', 'ADMIN', 'MALE', '2021-05-02 00:00:00.000',
         '2021-05-02 00:00:00.000'),
-       ('clvh9c4t30004356x7bandnpjZ', 'Jan', 'Novák',
+       ('clvh9c4t32004356x7bandnpjZ', 'Jan', 'Novák',
         '$argon2id$v=19$m=65536,t=3,p=4$dBtduH+22brzrY3Ogp+fXA$vkpdi9+F7f9Yi1kD45LCtoO1PVv8r8Rq2M0JFT0ch3o',
         'vlastabures13@gmail.com', '2000-11-02 00:00:00.000', 'STUDENT', 'MALE', '2021-05-02 00:00:00.000',
         '2021-05-02 00:00:00.000'),
