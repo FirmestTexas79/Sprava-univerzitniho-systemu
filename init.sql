5ae70244bc9ee704042c2e3725dcb7b2a614e3aa
noname <vlastaburak@seznam.cz>
2024-04-28 11:41:55 +0200

    změna id

--- init.sql
+++ init.sql
@@ -12,33 +12,33 @@ VALUES ('clvh9c4t30004356x7bandnpjA', 'Aplikovaná Informatika', '2021-05-02 00:
 -- password: Password1
 INSERT INTO `users` (`id`, `firstname`, `lastname`, `password`, `email`, `birthdate`, `role`, `sex`, `createdAt`,
                      `updatedAt`, `fieldOfStudyId`, `year`)
-VALUES ('clvh9c4t30004356x7bandnpjZ', 'Vlastimil', 'Bureš',
+VALUES ('clvh9c3t30004356x7bandnpjZ', 'Vlastimil', 'Bureš',
         '$argon2id$v=19$m=65536,t=3,p=4$dBtduH+22brzrY3Ogp+fXA$vkpdi9+F7f9Yi1kD45LCtoO1PVv8r8Rq2M0JFT0ch3o',
         'vlastaburak@seznam.cz', '2000-11-02 00:00:00.000', 'ADMIN', 'MALE', '2021-05-02 00:00:00.000',
         '2021-05-02 00:00:00.000', null, null),
-       ('clvh9c4t30004356x7bandnpjX', 'Jan', 'Novák',
+       ('clvh9c4t31004356x7bandnpjX', 'Jan', 'Novák',
         '$argon2id$v=19$m=65536,t=3,p=4$dBtduH+22brzrY3Ogp+fXA$vkpdi9+F7f9Yi1kD45LCtoO1PVv8r8Rq2M0JFT0ch3o',
         'vlastabures13@gmail.com', '2000-11-02 00:00:00.000', 'STUDENT', 'MALE', '2021-05-02 00:00:00.000',
         '2021-05-02 00:00:00.000', 'clvh9c4t30004356x7bandnpjA', null),
-       ('clvh9c4t30004356x7bandnpjY', 'Jana', 'Nováková',
+       ('clvh9c4t32004356x7bandnpjY', 'Jana', 'Nováková',
         '$argon2id$v=19$m=65536,t=3,p=4$dBtduH+22brzrY3Ogp+fXA$vkpdi9+F7f9Yi1kD45LCtoO1PVv8r8Rq2M0JFT0ch3o',
         'samurajcz222@gmail.com', '2000-11-02 00:00:00.000', 'TEACHER', 'FEMALE', '2021-05-02 00:00:00.000',
         '2021-05-02 00:00:00.000', null, null),
-       ('clvh9c4t30004356x7bandnpjW', 'Lukáš', 'Nový',
+       ('clvh9c4t33004356x7bandnpjW', 'Lukáš', 'Nový',
         '$argon2id$v=19$m=65536,t=3,p=4$dBtduH+22brzrY3Ogp+fXA$vkpdi9+F7f9Yi1kD45LCtoO1PVv8r8Rq2M0JFT0ch3o',
         'buresvl2@uhk.cz', '2000-11-02 00:00:00.000', 'TEACHER', 'MALE', '2021-05-02 00:00:00.000',
         '2021-05-02 00:00:00.000', null, null);
 
 INSERT INTO `subjects` (`id`, `name`, `createdAt`, `updatedAt`, `credits`, `shortName`, `category`, `department`,
                         `guarantorId`)
-VALUES ('clvh9c4t30004356x7bandnpj1', 'Programování 1', '2021-05-02 00:00:00.000', '2021-05-02 00:00:00.000', 5, 'PG1',
-        'BASIC', 'KIV', 'clvh9c4t30004356x7bandnpjY'),
-       ('clvh9c4t30004356x7bandnpj2', 'Matematika 1', '2021-05-02 00:00:00.000', '2021-05-02 00:00:00.000', 5, 'MAT1',
-        'BASIC', 'KIV', 'clvh9c4t30004356x7bandnpjW');
+VALUES ('clvh9c4t30104356x7bandnpj1', 'Programování 1', '2021-05-02 00:00:00.000', '2021-05-02 00:00:00.000', 5, 'PG1',
+        'BASIC', 'KIV', 'clvh9c4t32004356x7bandnpjY'),
+       ('clvh9c4t30204356x7bandnpj2', 'Matematika 1', '2021-05-02 00:00:00.000', '2021-05-02 00:00:00.000', 5, 'MAT1',
+        'BASIC', 'KIV', 'clvh9c4t33004356x7bandnpjW');
 
 INSERT INTO `rooms` (`id`, `name`, `createdAt`, `updatedAt`, `capacity`, `type`, `floor`)
-VALUES ('clvh9c4t30004356x7bandnpjR', 'A1', '2021-05-02 00:00:00.000', '2021-05-02 00:00:00.000', 50, 'LECTURE', 1),
-       ('clvh9c4t30004356x7bandnpjT', 'A2', '2021-05-02 00:00:00.000', '2021-05-02 00:00:00.000', 50, 'LECTURE', 1),
-       ('clvh9c4t30004356x7bandnpjU', 'A3', '2021-05-02 00:00:00.000', '2021-05-02 00:00:00.000', 18, 'COMPUTERS', 1),
-       ('clvh9c4t30004356x7bandnpjV', 'A4', '2021-05-02 00:00:00.000', '2021-05-02 00:00:00.000', 18, 'COMPUTERS', 1),
-       ('clvh9c4t30004356x7bandnpjZ', 'A5', '2021-05-02 00:00:00.000', '2021-05-02 00:00:00.000', 8, 'LABORATORY', 1);
+VALUES ('clvh9c4t30014356x7bandnpjR', 'A1', '2021-05-02 00:00:00.000', '2021-05-02 00:00:00.000', 50, 'LECTURE', 1),
+       ('clvh9c4t30024356x7bandnpjT', 'A2', '2021-05-02 00:00:00.000', '2021-05-02 00:00:00.000', 50, 'LECTURE', 1),
+       ('clvh9c4t30034356x7bandnpjU', 'A3', '2021-05-02 00:00:00.000', '2021-05-02 00:00:00.000', 18, 'COMPUTERS', 1),
+       ('clvh9c4t30044356x7bandnpjV', 'A4', '2021-05-02 00:00:00.000', '2021-05-02 00:00:00.000', 18, 'COMPUTERS', 1),
+       ('clvh9c4t30054356x7bandnpjZ', 'A5', '2021-05-02 00:00:00.000', '2021-05-02 00:00:00.000', 8, 'LABORATORY', 1);
