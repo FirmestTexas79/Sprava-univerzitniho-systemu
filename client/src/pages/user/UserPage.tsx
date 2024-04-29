94c0bf0c77ea26187cd26b1dd90547eb54c63b35
FirmestTexas79 <matasovsky.samuel@seznam.cz>
2024-04-29 07:16:33 +0200

    The end is comming, end comming with me

--- client/src/pages/user/UserPage.tsx
+++ client/src/pages/user/UserPage.tsx
@@ -14,8 +14,6 @@ import { NumberInput } from "../../components/inputs/NumberInput.tsx";
 import { SEX_OPTIONS, USER_ROLES_OPTIONS } from "../../services/utils.ts";
 import { FieldOfStudyApi } from "../../services/server/FieldOfStudyApi.ts";
 import { SortType } from "../../../../server/src/utils/sort-type.enum.ts";
-import { SubjectApi, UpdateSubjectForm } from "../../services/server/SubjectApi.ts";
-
 export default function UserPage() {
   const { id } = useParams();
   const {
@@ -23,7 +21,7 @@ export default function UserPage() {
     setUser,
     token,
   } = useAuth();
-  const [currentUser, setCurrentUser] = useState<User>();
+  const [currentUser, setCurrentUser] = useState<User>() ;
   const [errors, setErrors] = useState<Map<string | number, string>>();
   const [fieldOfStudy, setFieldOfStudy] = useState<FieldOfStudy[]>([]);
   const [loaded, setLoaded] = useState(false);
@@ -107,113 +105,113 @@ export default function UserPage() {
   }
 
   return (<Page>
-    <Container maxWidth="md">
-      <div className="form-container">
-        {user?.role !== UserRoles.ADMIN ? (<Box>
-          <Typography variant="h4">User</Typography>
-          <Typography variant="h6">Email: {currentUser?.email}</Typography>
-          <Typography variant="h6">Role: {currentUser?.role}</Typography>
-          <Typography variant="h6">Firstname: {currentUser?.firstname}</Typography>
-          <Typography variant="h6">Lastname: {currentUser?.lastname}</Typography>
-          <Typography variant="h6">Birthdate: {currentUser?.birthdate.toLocaleDateString()}</Typography>
-          <Typography variant="h6">Title after: {currentUser?.titleAfter}</Typography>
-          <Typography variant="h6">Title before: {currentUser?.titleBefore}</Typography>
-          {user?.role === UserRoles.STUDENT && (<>
-            <Typography variant="h6">Phone: {currentUser?.phone}</Typography>
-            <Typography variant="h6">Year: {currentUser?.year}</Typography>
-          </>)}
-          <Typography variant="h6">Field of study: {currentUser?.fieldOfStudyId}</Typography>
-          <Typography variant="h6">Sex: {currentUser?.sex}</Typography>
-        </Box>) : (<Box>
-          <Typography variant="h4">User</Typography>
-          <Typography variant="h6">ID: {currentUser?.id}</Typography>
-          <TextInput
-              onChange={(value) => onChange("email", value)}
-              error={errors?.has("email")}
-              helperText={errors?.get("email")}
-              type={"email"}
-              value={currentUser?.email}
-              label="Email"/>
-          <TextInput
-              onChange={(value) => onChange("firstname", value)}
-              error={errors?.has("firstname")}
-              helperText={errors?.get("firstname")}
-              value={currentUser?.firstname}
-              label="Jméno"/>
-          <TextInput
-              onChange={(value) => onChange("lastname", value)}
-              error={errors?.has("lastname")}
-              helperText={errors?.get("lastname")}
-              value={currentUser?.lastname}
-              label="Přijmení"/>
-          <DateInput
-              onChange={(value) => onChange("birthdate", value)}
-              error={errors?.has("birthdate")}
-              disableFuture
-              helperText={errors?.get("birthdate")}
-              value={currentUser?.birthdate}
-              label="Datum narození"/>
-          <TextInput
-              onChange={(value) => onChange("titleAfter", value)}
-              error={errors?.has("titleAfter")}
-              helperText={errors?.get("titleAfter")}
-              value={currentUser?.titleAfter}
-              label="Titul po"/>
-          <TextInput
-              onChange={(value) => onChange("titleBefore", value)}
-              error={errors?.has("titleBefore")}
-              helperText={errors?.get("titleBefore")}
-              value={currentUser?.titleBefore}
-              label="Titul před"/>
-          <TextInput
-              onChange={(value) => onChange("phone", value)}
-              type={"tel"}
-              error={errors?.has("phone")}
-              helperText={errors?.get("phone")}
-              value={currentUser?.phone}
-              label="Telefon"/>
-          <SelectInput
-              options={SEX_OPTIONS}
-              onChange={(value) => onChange("sex", value)}
-              error={errors?.has("sex")}
-              helperText={errors?.get("sex")}
-              value={currentUser?.sex}
-              label="Pohlaví"/>
-          <SelectInput
-              options={USER_ROLES_OPTIONS}
-              onChange={(value) => onChange("role", value)}
-              error={errors?.has("role")}
-              helperText={errors?.get("role")}
-              value={currentUser?.role}
-              label="Role"/>
-          {currentUser?.role === UserRoles.STUDENT && (
-              <>
-                <NumberInput
-                    onChange={(value) => onChange("year", value)}
-                    error={errors?.has("year")}
-                    helperText={errors?.get("year")}
-                    value={currentUser?.year}
-                    label="Ročník"/>
-                <SelectInput
-                    options={fieldOfStudy?.map((value) => ({
-                      label: value.name + " - " + value.type,
-                      value: value.id,
-                    } as Option<string>)) || []}
-                    onOpen={() => getFieldOfStudyOptions()}
-                    onChange={(value) => onChange("fieldOfStudyId", value)}
-                    error={errors?.has("fieldOfStudyId")}
-                    helperText={errors?.get("fieldOfStudyId")}
-                    value={currentUser?.fieldOfStudyId}
-                    label="Obor"/>
-              </>
-          )}
-          <Button
-              variant="contained"
-              fullWidth
-              onClick={onSubmit}>Uložit</Button>
-        </Box>)}
-      </div>
-    </Container>
-  </Page>
-);
+        <Container>
+          <div className="form-container-cerm">
+            {user?.role !== UserRoles.ADMIN ? (<Box>
+              <Typography variant="h4">User</Typography>
+              <Typography variant="h6">Email: {currentUser?.email}</Typography>
+              <Typography variant="h6">Role: {currentUser?.role}</Typography>
+              <Typography variant="h6">Firstname: {currentUser?.firstname}</Typography>
+              <Typography variant="h6">Lastname: {currentUser?.lastname}</Typography>
+              <Typography variant="h6">Birthdate: {currentUser?.birthdate.toLocaleDateString()}</Typography>
+              <Typography variant="h6">Title after: {currentUser?.titleAfter}</Typography>
+              <Typography variant="h6">Title before: {currentUser?.titleBefore}</Typography>
+              {user?.role === UserRoles.STUDENT && (<>
+                <Typography variant="h6">Phone: {currentUser?.phone}</Typography>
+                <Typography variant="h6">Year: {currentUser?.year}</Typography>
+              </>)}
+              <Typography variant="h6">Field of study: {currentUser?.fieldOfStudyId}</Typography>
+              <Typography variant="h6">Sex: {currentUser?.sex}</Typography>
+            </Box>) : (<Box>
+              <Typography variant="h4">User</Typography>
+              <Typography variant="h6">ID: {currentUser?.id}</Typography>
+              <TextInput
+                  onChange={(value) => onChange("email", value)}
+                  error={errors?.has("email")}
+                  helperText={errors?.get("email")}
+                  type={"email"}
+                  value={currentUser?.email}
+                  label="Email"/>
+              <TextInput
+                  onChange={(value) => onChange("firstname", value)}
+                  error={errors?.has("firstname")}
+                  helperText={errors?.get("firstname")}
+                  value={currentUser?.firstname}
+                  label="Jméno"/>
+              <TextInput
+                  onChange={(value) => onChange("lastname", value)}
+                  error={errors?.has("lastname")}
+                  helperText={errors?.get("lastname")}
+                  value={currentUser?.lastname}
+                  label="Přijmení"/>
+              <DateInput
+                  onChange={(value) => onChange("birthdate", value)}
+                  error={errors?.has("birthdate")}
+                  disableFuture
+                  helperText={errors?.get("birthdate")}
+                  value={currentUser?.birthdate}
+                  label="Datum narození"/>
+              <TextInput
+                  onChange={(value) => onChange("titleAfter", value)}
+                  error={errors?.has("titleAfter")}
+                  helperText={errors?.get("titleAfter")}
+                  value={currentUser?.titleAfter}
+                  label="Titul po"/>
+              <TextInput
+                  onChange={(value) => onChange("titleBefore", value)}
+                  error={errors?.has("titleBefore")}
+                  helperText={errors?.get("titleBefore")}
+                  value={currentUser?.titleBefore}
+                  label="Titul před"/>
+              <TextInput
+                  onChange={(value) => onChange("phone", value)}
+                  type={"tel"}
+                  error={errors?.has("phone")}
+                  helperText={errors?.get("phone")}
+                  value={currentUser?.phone}
+                  label="Telefon"/>
+              <SelectInput
+                  options={SEX_OPTIONS}
+                  onChange={(value) => onChange("sex", value)}
+                  error={errors?.has("sex")}
+                  helperText={errors?.get("sex")}
+                  value={currentUser?.sex}
+                  label="Pohlaví"/>
+              <SelectInput
+                  options={USER_ROLES_OPTIONS}
+                  onChange={(value) => onChange("role", value)}
+                  error={errors?.has("role")}
+                  helperText={errors?.get("role")}
+                  value={currentUser?.role}
+                  label="Role"/>
+              {currentUser?.role === UserRoles.STUDENT && (
+                  <>
+                    <NumberInput
+                        onChange={(value) => onChange("year", value)}
+                        error={errors?.has("year")}
+                        helperText={errors?.get("year")}
+                        value={currentUser?.year}
+                        label="Ročník"/>
+                    <SelectInput
+                        options={fieldOfStudy?.map((value) => ({
+                          label: value.name + " - " + value.type,
+                          value: value.id,
+                        } as Option<string>)) || []}
+                        onOpen={() => getFieldOfStudyOptions()}
+                        onChange={(value) => onChange("fieldOfStudyId", value)}
+                        error={errors?.has("fieldOfStudyId")}
+                        helperText={errors?.get("fieldOfStudyId")}
+                        value={currentUser?.fieldOfStudyId}
+                        label="Obor"/>
+                  </>
+              )}
+              <Button
+                  variant="contained"
+                  fullWidth
+                  onClick={onSubmit}>Uložit</Button>
+            </Box>)}
+          </div>
+        </Container>
+      </Page>
+  );
 }
